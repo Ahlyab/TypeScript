1962973fb501b28810e29419dba1f5f111359b5d
Ahlyab <ahlyabasad@gmail.com>
2025-11-30 23:23:17 +0500

    Changes to be committed: 	modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,32 @@
-let age = 20;
-if(age > 18) {
-    age += 10;
+// let age = 20;
+// if(age > 18) {
+//     age += 10;
+// }
+// console.log(age);
+
+// tuples
+
+// let user: [number, string] = [1, 'test'];
+
+// user.push(10);
+
+// enum
+
+// const enum Size {
+//     Small = 1,
+//     Medium = 2,
+//     Large = 3
+// };
+
+// const mySize: Size = Size.Medium;
+
+// console.log(mySize)
+
+// functions
+
+function calculateTax(income: number) : number {
+    if(income > 50_000) {
+    return income * 1.2;    
+    }
+    return income * 1.3;
 }
-console.log(age);
